d6c4a89270f590407619958c2532bc17dee9f89c
Bryce Kalmbach <jbkalmbach@gmail.com>
2017-03-14 23:21:50 -0700

    Added spec_utils tests.

---
+++ code/spec_utils.py
@@ -0,0 +1,62 @@
+import os
+import math
+import numpy as np
+from lsst_utils.Sed import Sed
+
+
+class specUtils(object):
+    """Contains utilities for package classes."""
+
+    def load_spectra(self, directory):
+        """
+        Read in spectra from files in given directory.
+
+        Parameters
+        ----------
+        directory: str
+            Location of spectra files.
+
+        Returns
+        -------
+        spec_list: list of Sed class instances
+            List where each entry is an instance of the Sed class containing
+            the information for one model spectrum.
+        """
+        spec_list = []
+        for root, dirs, files in os.walk(directory):
+            file_total = len(files)
+            file_on = 1
+            for name in files:
+                if file_on % 100 == 0:
+                    print str("File On " + str(file_on) + " out of " +
+                              str(file_total))
+                file_on += 1
+                try:
+                    spec = Sed()
+                    spec.readSED_flambda(os.path.join(root, name))
+                    spec.name = name
+                    if math.isnan(spec.flambda[0]) is False:
+                        spec_list.append(spec)
+                except:
+                    continue
+
+        return spec_list
+
+    def scale_spectrum(self, sed_flux):
+        """
+        Norm spectrum so adds up to 1.
+
+        Parameters
+        ----------
+        sedFlux: array
+            The flux array of an SED.
+
+        Returns
+        -------
+        norm_spec: array
+            The normalized flux array.
+        """
+        norm = np.sum(sed_flux)
+        norm_spec = sed_flux/norm
+
+        return norm_spec
---
+++ tests/test_spec_utils.py
@@ -0,0 +1,49 @@
+import unittest
+import os
+import shutil
+import numpy as np
+from spec_utils import specUtils
+
+
+class testSpecUtils(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        sample_wavelen = np.arange(50)
+        sample_flux = np.random.uniform(size=50)
+
+        cls.sample_spec = np.array([sample_wavelen, sample_flux])
+
+        if os.path.exists('scratch'):
+            shutil.rmtree('scratch')
+
+        os.mkdir('scratch')
+        np.savetxt('scratch/sample.dat', cls.sample_spec.T,
+                   header='Lambda Flux', delimiter=' ')
+
+    def test_load_spectra(self):
+
+        test_su = specUtils()
+        test_spec = test_su.load_spectra('scratch')
+
+        self.assertItemsEqual(self.sample_spec[0], test_spec[0].wavelen)
+        self.assertItemsEqual(self.sample_spec[1], test_spec[0].flambda)
+
+    def test_scale_spectrum(self):
+
+        test_su = specUtils()
+        test_spec = test_su.load_spectra('scratch')
+        new_flux = test_su.scale_spectrum(test_spec[0].flambda)
+        self.assertAlmostEqual(np.sum(new_flux), 1.0)
+
+    @classmethod
+    def tearDownClass(cls):
+
+        if os.path.exists('scratch'):
+            shutil.rmtree('scratch')
+
+
+if __name__ == '__main__':
+
+    unittest.main()
