5f43f5dcf28c2dcec9006234ff0c1e16b2f384c1
Bryce Kalmbach <jbkalmbach@gmail.com>
2017-03-17 15:58:04 -0700

    Adjusted test_spec_utils

--- tests/test_spec_utils.py
+++ tests/test_spec_utils.py
@@ -15,34 +15,36 @@ class testSpecUtils(unittest.TestCase):
 
         cls.sample_spec = np.array([sample_wavelen, sample_flux])
 
-        if os.path.exists('scratch'):
-            shutil.rmtree('scratch')
+        if os.path.exists('scratch_spec'):
+            shutil.rmtree('scratch_spec')
 
-        os.mkdir('scratch')
-        np.savetxt('scratch/sample.dat', cls.sample_spec.T,
+        os.mkdir('scratch_spec')
+        np.savetxt('scratch_spec/sample.dat', cls.sample_spec.T,
                    header='Lambda Flux', delimiter=' ')
 
     def test_load_spectra(self):
 
         test_su = specUtils()
-        test_spec = test_su.load_spectra('scratch')
+        test_spec = test_su.load_spectra('scratch_spec')
 
-        self.assertItemsEqual(self.sample_spec[0], test_spec[0].wavelen)
-        self.assertItemsEqual(self.sample_spec[1], test_spec[0].flambda)
+        np.testing.assert_array_equal(self.sample_spec[0],
+                                      test_spec[0].wavelen)
+        np.testing.assert_array_equal(self.sample_spec[1],
+                                      test_spec[0].flambda)
         self.assertEqual('sample.dat', test_spec[0].name)
 
     def test_scale_spectrum(self):
 
         test_su = specUtils()
-        test_spec = test_su.load_spectra('scratch')
+        test_spec = test_su.load_spectra('scratch_spec')
         new_flux = test_su.scale_spectrum(test_spec[0].flambda)
         self.assertAlmostEqual(np.sum(new_flux), 1.0)
 
     @classmethod
     def tearDownClass(cls):
 
-        if os.path.exists('scratch'):
-            shutil.rmtree('scratch')
+        if os.path.exists('scratch_spec'):
+            shutil.rmtree('scratch_spec')
 
 
 if __name__ == '__main__':
