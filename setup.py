1c7edfb0801196d1283acfbb7d265354a7c5877e
Bryce Kalmbach <jbkalmbach@gmail.com>
2017-10-27 15:46:46 -0700

    setup.py running. Removed setup folder.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="esp",
+    name="esphot",
     version="0.1",
     author="Bryce Kalmbach",
     author_email="brycek@uw.edu",
--- setup/setup.sh
+++
@@ -1,2 +0,0 @@
-package_dir=$( cd $(dirname $BASH_SOURCE)/..; pwd -P )
-export PYTHONPATH=${package_dir}/:$PYTHONPATH
