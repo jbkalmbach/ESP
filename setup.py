83d7f7fa5f64653956530985ddfa8ce37b2c8182
Bryce Kalmbach <jbkalmbach@gmail.com>
2017-10-30 10:06:46 -0700

    Paper notebook added.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="esphot",
-    version="0.1.0",
+    version="0.2.0",
     author="Bryce Kalmbach",
     author_email="brycek@uw.edu",
     url="https://github.com/jbkalmbach/esp",
